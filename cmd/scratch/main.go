ab354605bad68e7f58ff4064b247d6091864b67f
Kevin Greenan <kmgreen2@gmail.com>
2021-01-09 15:13:11 -0800

    Fixed scratch binary

--- cmd/scratch/main.go
+++ cmd/scratch/main.go
@@ -1,36 +1,5 @@
 package main
 
-import (
-	"context"
-	"github.com/kmgreen2/agglo/pkg/observability"
-)
-
 func main() {
-	exporter, err := observability.NewStdoutExporter(observability.StdoutExporter)
-	if err != nil {
-		panic(err)
-	}
-	_ = exporter.Start()
-	defer func() {_ = exporter.Stop()}()
-
-	emitter := observability.NewEmitter("foo.io")
-	emitter.AddMetric("fizz", observability.Int64Counter)
-	emitter.AddMetric("buzz", observability.Int64Recorder)
-	ctx1, span1 := emitter.CreateSpan(context.Background(), "foo")
-	ctx2, span2 := emitter.CreateSpan(ctx1, "bar")
-	_, span3 := emitter.CreateSpan(ctx2, "baz")
-
-	span1.AddEvent("Did a thing")
-
-	emitter.AddInt64("fizz", 1)
-	emitter.AddInt64("fizz", 1)
-	emitter.AddInt64("fizz", 1)
-	emitter.AddInt64("fizz", 1)
-	emitter.AddInt64("fizz", 1)
-	emitter.AddInt64("fizz", 1)
-
-	span1.End()
-	span2.End()
-	span3.End()
 }
 
