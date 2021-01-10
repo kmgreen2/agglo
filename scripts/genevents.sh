258527ce0832cf96ba0788dc929fad10e6283a95
Kevin Greenan <kmgreen2@gmail.com>
2021-01-09 17:57:38 -0800

    Adding zipkin docker compose files and helpers for running load tests

---
+++ scripts/genevents.sh
@@ -0,0 +1,1 @@
+kmgreen$ bin/genevents -numEvents 10 -numThreads 10 -schema test/config/cicd_events.json  -output http://localhost:80/binge
---
+++ scripts/run-binge.sh
@@ -0,0 +1,1 @@
+ bin/binge  -daemonPath /binge -daemonPort 80 -maxConnections 16 -runType stateless-daemon -exporter zipkin -config test/config/cicd_pipeline.json  
