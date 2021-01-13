b5b59ee4269a13367e5daded29cf75359cf739da
Kevin Greenan <kmgreen2@gmail.com>
2021-01-13 14:43:17 -0800

    Added more text

--- scripts/genevents.sh
+++ scripts/genevents.sh
@@ -1,1 +1,1 @@
-bin/genevents -numEvents 16 -numThreads 16 -schema test/generators/cicd_events.json  -output http://localhost:80/binge
+bin/genevents -numEvents 100 -numThreads 16 -schema test/generators/cicd_events.json  -output http://localhost:80/binge
