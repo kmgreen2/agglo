08d788b051c14f1cc300a1ed122502717588a723
Kevin Greenan <kmgreen2@gmail.com>
2021-02-18 17:53:18 -0800

    issue-17: Adding PEM parsers

--- pkg/crypto/rsa_auth.go
+++ pkg/crypto/rsa_auth.go
@@ -5,6 +5,7 @@ import (
 	"crypto/rsa"
 	"crypto/rand"
 	"crypto/x509"
+	"encoding/pem"
 	"github.com/kmgreen2/agglo/pkg/util"
 	"hash"
 	"reflect"
@@ -174,4 +175,62 @@ func DeserialilzeRSAPrivateKey(bytes []byte) (*rsa.PrivateKey, error) {
 	}
 
 	return privateKey, err
+}
+
+func ExportRSAPrivateKeyAsPEM(privateKey *rsa.PrivateKey) string {
+	privateKeyBytes := x509.MarshalPKCS1PrivateKey(privateKey)
+	privateKeyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PRIVATE KEY",
+			Bytes: privateKeyBytes,
+		},
+	)
+	return string(privateKeyPEM)
+}
+
+func ParseRSAPrivateKeyFromPEM(privateKeyPEM string) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode([]byte(privateKeyPEM))
+	if block == nil {
+		return nil, util.NewInvalidError("failed to parse PEM block containing the key")
+	}
+
+	privateKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return privateKey, nil
+}
+
+func ExportRSAPublicKeyAsPEM(publicKey *rsa.PublicKey) (string, error) {
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(publicKey)
+	if err != nil {
+		return "", err
+	}
+	publicKeyPEM := pem.EncodeToMemory(
+		&pem.Block{
+			Type:  "RSA PUBLIC KEY",
+			Bytes: publicKeyBytes,
+		},
+	)
+
+	return string(publicKeyPEM), nil
+}
+
+func ParseRSAPublicKeyFromPEM(publicKeyPEM string) (*rsa.PublicKey, error) {
+	block, _ := pem.Decode([]byte(publicKeyPEM))
+	if block == nil {
+		return nil, util.NewInvalidError("failed to parse PEM block containing the key")
+	}
+
+	pub, err := x509.ParsePKIXPublicKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+
+	switch pub := pub.(type) {
+	case *rsa.PublicKey:
+		return pub, nil
+	}
+	return nil, util.NewInvalidError("Key type is not RSA")
 }
